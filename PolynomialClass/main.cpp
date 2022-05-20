15cb18ec8d686cc33a64554ff8659d2a50d0e1d6
Noah LaPoint <noah.lapoint@icloud.com>
2022-05-20 16:55:37 -0400

    Initial Commit

---
+++ PolynomialClass/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PolynomialClass
+//
+//  Created by Noah LaPoint on 5/20/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
